f2dff3e79da3eb5fb97b4b923b5bf0fade157f53
JameicaAlvarenga <jnbrag@gmail.com>
2021-07-29 13:26:24 -0300

    refatorando classe pessoa

--- src/main/kotlin/digio/onebank/Pessoa.kt
+++ src/main/kotlin/digio/onebank/Pessoa.kt
@@ -1,19 +1,7 @@
 package digio.onebank
 
 
-class Pessoa {
-    var nome: String = "Jam"
-    var cpf: String = "123.456.789-23"
-    private set
-
-    constructor()
-
-    fun infoPessoa() = "$nome e $cpf"
-}
-
-fun main(){
-    val jame = Pessoa()
-
-    println(jame.infoPessoa())
-
-}
+open class Pessoa (
+    open val nome: String,
+    open val cpf: String
+)
