ff6f1db7ee3e60ac2e4ed741fbf46e7183d40196
JameicaAlvarenga <jnbrag@gmail.com>
2021-07-28 21:39:57 -0300

    criado construtor secundário

--- src/main/kotlin/digio/onebank/Pessoa.kt
+++ src/main/kotlin/digio/onebank/Pessoa.kt
@@ -5,11 +5,15 @@ class Pessoa {
     var nome: String = "Jam"
     var cpf: String = "123.456.789-23"
     private set
+
+    constructor()
+
+    fun infoPessoa() = "$nome e $cpf"
 }
 
 fun main(){
     val jame = Pessoa()
 
-    println(jame.nome)
-    println(jame.cpf)
+    println(jame.infoPessoa())
+
 }
