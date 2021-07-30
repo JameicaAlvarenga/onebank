ce4b97f64271752dbc41f5b8b310d1c9fdff86fb
JameicaAlvarenga <jnbrag@gmail.com>
2021-07-30 08:05:44 -0300

    criando classe Analista com teste

--- src/main/kotlin/digio/onebank/testes/TesteFuncionarioAnalista.kt
+++ src/main/kotlin/digio/onebank/testes/TesteFuncionarioAnalista.kt
@@ -6,7 +6,7 @@ import digio.onebank.Pessoa
 import java.math.BigDecimal
 
 fun main() {
-    val jam = Analista("Jameica", "1235678956", 2000.0)
+    val jam = Analista("Jameica", "1235678956", 6000.0)
     ImprimeRelatorioFuncionario.imprime(jam)
 }
 
